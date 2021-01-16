2d82bb041f1e87b91ab8e2ca77922faf87b50b34
EnesGedik <77547476+EnesGedik@users.noreply.github.com>
2021-01-17 00:14:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace methots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Name = "Elma";
+            urun1.Price = 35;
+            urun1.Aciklma = "Ankara elmasi";
+            urun1.StokAdedi = 10;
+
+            Urun urun2 = new Urun();
+            urun2.Name = "Karpuz";
+            urun2.Price = 55;
+            urun2.Aciklma = "Diyarbakir karpuzu";
+            urun2.StokAdedi = 5;
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler) 
+            {
+                Console.WriteLine(urun.Name);
+                Console.WriteLine(urun.Price);
+                Console.WriteLine(urun.Aciklma);
+                Console.WriteLine(urun.StokAdedi);
+                Console.WriteLine("------------------");
+            
+            }
+
+            SepetMeneger sepetManager = new SepetMeneger();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+        }
+    }
+}
---
+++ SepetMeneger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methots
+{
+    class SepetMeneger
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("urun basariyla sepete eklendi" + " " + urun.Name);
+
+
+        }
+    }
+}
